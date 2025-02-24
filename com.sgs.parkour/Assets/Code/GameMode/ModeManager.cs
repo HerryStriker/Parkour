84449ec0410e785148256d9f0258909323b5a5c8
Lucas Vicente <dasilvavicentelucas@gmail.com>
2025-02-23 23:30:52 -0500

    Re-commit everything on local for better track.

---
+++ com.sgs.parkour/Assets/Code/GameMode/ModeManager.cs
@@ -0,0 +1,33 @@
+using Parkour.State;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class ModeManager : MonoBehaviour
+{   
+    public static ModeManager Instance;
+
+    [SerializeField] StateObject<ModeType> _mode;
+    [SerializeField] TimeCounter timeCounter;
+    
+    void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        _mode = new StateObject<ModeType>(ModeType.NONE);
+        timeCounter = new TimeCounter();
+    }
+
+    public void StartGame()
+    {
+        timeCounter.Start(this);
+    }
+    
+    public StateObject<ModeType> Mode => _mode;
+}
+public enum ModeType
+{
+    NONE,
+    PLATFORM2D,
+    RUSH,
+    INTOTOP
+}
---
+++ com.sgs.parkour/Assets/Code/Utils/TimeCounter.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[System.Serializable]
+public class TimeCounter
+{
+    [SerializeField] bool isCounting = false;
+    [SerializeField] float h,m,s;
+    [SerializeField] float incrementTime = 1f;
+    const float MAX_TIME_UNITY = 59;
+    public IEnumerator IncrementTimer()
+    {
+        do {
+            s += incrementTime;
+
+            if(s > MAX_TIME_UNITY)
+            {
+                s = 0;
+                m += 1;
+
+                if(m > MAX_TIME_UNITY)
+                {
+                    m = 0;
+                    h += 1;
+
+                    if(h > MAX_TIME_UNITY)
+                    {
+                        s = 0;
+                        m = 0;
+                        h = 0;
+                        isCounting = false;
+                    }
+                }
+            }
+            yield return new WaitForSecondsRealtime(1f);
+        }
+        while (isCounting);
+    }
+
+    public void Start(MonoBehaviour monoBehaviour)
+    {
+        Reset();
+        isCounting = true;
+        
+        monoBehaviour.StartCoroutine(IncrementTimer());
+    }
+
+    void Reset()
+    {
+        s = 0;
+        m = 0;
+        h = 0;
+        isCounting = false;
+        
+    }
+}
